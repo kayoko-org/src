66533d57c49d819428d34f01f5b349041ea14d79
Charlie Root <root@xai.srclo.net>
2026-03-27 20:29:58 +0000

    refine sleep.c

--- src/cmd/core/sleep.c
+++ src/cmd/core/sleep.c
@@ -1,18 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <ctype.h>
 
 int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        fprintf(stderr, "usage: %s seconds\n", argv[0]);
+    if (argc != 2) {
+        fprintf(stderr, "usage: sleep seconds\n");
         return 1;
     }
 
-    /* POSIX: seconds must be a non-negative integer */
+    // POSIX check: Ensure it's actually a decimal integer string
+    for (char *p = argv[1]; *p; p++) {
+        if (!isdigit((unsigned char)*p)) {
+            fprintf(stderr, "usage: sleep seconds\n");
+            return 1;
+        }
+    }
+
     unsigned int seconds = (unsigned int)strtoul(argv[1], NULL, 10);
-    
-    /* sleep() returns the number of unslept seconds if interrupted */
-    while ((seconds = sleep(seconds)) > 0);
+
+    // POSIX Utility spec: If interrupted by a caught signal, 
+    // it should just stop and exit 0. 
+    // The loop is great for a library, but the utility usually just calls it once.
+    sleep(seconds);
 
     return 0;
 }
