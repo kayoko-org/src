d1bd420b71440f2f08fe5f1dd53bc7f9b0f3b1c5
Charlie Root <root@xai.srclo.net>
2026-03-27 20:22:06 +0000

    mktemp

---
+++ src/cmd/core/mktemp.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+    int dflag = 0;
+    char *template;
+    int opt;
+
+    while ((opt = getopt(argc, argv, "d")) != -1) {
+        if (opt == 'd') dflag = 1;
+        else return 1;
+    }
+
+    if (optind < argc) {
+        template = argv[optind];
+    } else {
+        template = strdup("/tmp/fileXXXXXX");
+    }
+
+    if (dflag) {
+        if (!mkdtemp(template)) {
+            perror("mktemp");
+            return 1;
+        }
+    } else {
+        int fd = mkstemp(template);
+        if (fd < 0) {
+            perror("mktemp");
+            return 1;
+        }
+        close(fd);
+    }
+
+    puts(template);
+    return 0;
+}
