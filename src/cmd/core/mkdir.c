3be3225a2e100225534d1eba1345ced0095423bc
Charlie Root <root@xai.srclo.net>
2026-03-27 01:39:08 +0000

    arghs

---
+++ src/cmd/core/mkdir.c
@@ -0,0 +1,79 @@
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+static int mkpath(char *path, mode_t mode);
+
+void usage(void) {
+    fprintf(stderr, "usage: mkdir [-p] [-m mode] directory ...\n");
+    exit(1);
+}
+
+int main(int argc, char *argv[]) {
+    int pflag = 0;
+    mode_t mode = 0777;
+    int ch, error = 0;
+    char *ep;
+
+    while ((ch = getopt(argc, argv, "pm:")) != -1) {
+        switch (ch) {
+        case 'p':
+            pflag = 1;
+            break;
+        case 'm':
+            mode = strtol(optarg, &ep, 8);
+            if (*ep != '\0') {
+                fprintf(stderr, "mkdir: invalid file mode: %s\n", optarg);
+                exit(1);
+            }
+            break;
+        default:
+            usage();
+        }
+    }
+
+    argc -= optind;
+    argv += optind;
+
+    if (argc < 1) usage();
+
+    for (; *argv; ++argv) {
+        if (pflag) {
+            if (mkpath(*argv, mode) != 0) error = 1;
+        } else {
+            if (mkdir(*argv, mode) != 0) {
+                perror(*argv);
+                error = 1;
+            }
+        }
+    }
+
+    return error;
+}
+
+static int mkpath(char *path, mode_t mode) {
+    char *p;
+    struct stat sb;
+
+    for (p = path + 1; *p; p++) {
+        if (*p == '/') {
+            *p = '\0';
+            if (stat(path, &sb) != 0) {
+                if (mkdir(path, S_IRWXU | S_IRWXG | S_IRWXO) != 0 && errno != EEXIST) {
+                    perror(path);
+                    return -1;
+                }
+            }
+            *p = '/';
+        }
+    }
+    if (mkdir(path, mode) != 0 && errno != EEXIST) {
+        perror(path);
+        return -1;
+    }
+    return 0;
+}
---
+++ src/cmd/core/rmdir.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+
+void usage(void) {
+    fprintf(stderr, "usage: rmdir [-p] directory ...\n");
+    exit(1);
+}
+
+int main(int argc, char *argv[]) {
+    int pflag = 0;
+    int ch, error = 0;
+
+    while ((ch = getopt(argc, argv, "p")) != -1) {
+        switch (ch) {
+        case 'p':
+            pflag = 1;
+            break;
+        default:
+            usage();
+        }
+    }
+
+    argc -= optind;
+    argv += optind;
+
+    if (argc < 1) usage();
+
+    for (; *argv; ++argv) {
+        char *path = *argv;
+        
+        if (rmdir(path) != 0) {
+            perror(path);
+            error = 1;
+            continue;
+        }
+
+        if (pflag) {
+            char *p;
+            while ((p = strrchr(path, '/')) != NULL) {
+                *p = '\0';
+                if (*path == '\0') break; // Reached root
+                if (rmdir(path) != 0) {
+                    // POSIX says pflag should not report errors for parents 
+                    // if they aren't empty, just stop.
+                    break;
+                }
+            }
+        }
+    }
+
+    return error;
+}
