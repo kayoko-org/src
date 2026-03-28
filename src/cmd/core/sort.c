0cef1f16fc5cf5d03e6efeeed606938c16ff8db8
Charlie Root <root@xai.srclo.net>
2026-03-28 00:32:00 +0000

    make sort more POSIX

--- src/cmd/core/sort.c
+++ src/cmd/core/sort.c
@@ -1,21 +1,40 @@
+#define _POSIX_C_SOURCE 200809L
+#define _XOPEN_SOURCE 700
+#define _FILE_OFFSET_BITS 64
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <locale.h>
+#include <errno.h>
 
-typedef struct { char *data; } Line;
+typedef struct {
+    char *data;
+} Line;
 
 int rev = 1;
 int num = 0;
 
+void usage(const char *prog) {
+    fprintf(stderr, "usage: %s [-cnru] [-o output] [file ...]\n", prog);
+    exit(2);
+}
+
 int compare_lines(const void *a, const void *b) {
-    const char *s1 = ((Line *)a)->data;
-    const char *s2 = ((Line *)b)->data;
-    
+    const char *s1 = ((const Line *)a)->data;
+    const char *s2 = ((const Line *)b)->data;
+    int res = 0;
+
     if (num) {
-        return rev * (atof(s1) - atof(s2) > 0 ? 1 : (atof(s1) - atof(s2) < 0 ? -1 : 0));
+        char *end1, *end2;
+        double d1 = strtod(s1, &end1);
+        double d2 = strtod(s2, &end2);
+        res = (d1 > d2) ? 1 : (d1 < d2 ? -1 : 0);
+    } else {
+        res = strcoll(s1, s2);
     }
-    return rev * strcmp(s1, s2);
+    return rev * res;
 }
 
 void load_file(FILE *fp, Line **lines, size_t *count, size_t *cap) {
@@ -23,38 +42,55 @@ void load_file(FILE *fp, Line **lines, size_t *count, size_t *cap) {
     size_t len = 0;
     while (getline(&curr, &len, fp) != -1) {
         if (*count >= *cap) {
-            *cap *= 2;
-            *lines = realloc(*lines, *cap * sizeof(Line));
+            size_t new_cap = (*cap == 0) ? 1024 : *cap * 2;
+            Line *temp = realloc(*lines, new_cap * sizeof(Line));
+            if (!temp) {
+                perror("sort: memory exhausted");
+                exit(1);
+            }
+            *lines = temp;
+            *cap = new_cap;
         }
-        (*lines)[(*count)++].data = strdup(curr);
+        (*lines)[*count].data = strdup(curr);
+        if (!(*lines)[*count].data) {
+            perror("sort: strdup");
+            exit(1);
+        }
+        (*count)++;
     }
     free(curr);
 }
 
 int main(int argc, char *argv[]) {
-    size_t cap = 1024, count = 0;
-    Line *lines = malloc(cap * sizeof(Line));
-    int unique = 0, check = 0, merge = 0, opt;
+    setlocale(LC_ALL, "");
+
+    size_t cap = 0, count = 0;
+    Line *lines = NULL;
+    int unique = 0, check = 0, opt;
     char *outfile = NULL;
 
-    while ((opt = getopt(argc, argv, "nmcu ro:")) != -1) {
+    /* Handle unknown flags strictly */
+    while ((opt = getopt(argc, argv, "cnruo:")) != -1) {
         switch (opt) {
-            case 'n': num = 1; break;
-            case 'm': merge = 1; break; /* In memory, merge/sort are effectively similar */
             case 'c': check = 1; break;
+            case 'n': num = 1;   break;
+            case 'r': rev = -1;  break;
             case 'u': unique = 1; break;
-            case 'r': rev = -1; break;
             case 'o': outfile = optarg; break;
+            default:  usage(argv[0]);
         }
     }
 
-    /* Process input operands */
+    /* Input Processing */
     if (optind == argc) {
         load_file(stdin, &lines, &count, &cap);
     } else {
         for (int i = optind; i < argc; i++) {
             FILE *fp = (strcmp(argv[i], "-") == 0) ? stdin : fopen(argv[i], "r");
-            if (!fp) continue;
+            if (!fp) {
+                fprintf(stderr, "sort: %s: %s\n", argv[i], strerror(errno));
+                exit(1);
+            }
             load_file(fp, &lines, &count, &cap);
             if (fp != stdin) fclose(fp);
         }
@@ -62,14 +98,25 @@ int main(int argc, char *argv[]) {
 
     if (check) {
         for (size_t i = 1; i < count; i++) {
-            if (compare_lines(&lines[i-1], &lines[i]) > 0) return 1;
+            if (compare_lines(&lines[i-1], &lines[i]) > 0) {
+                fprintf(stderr, "sort: disorder: %s", lines[i].data);
+                return 1;
+            }
         }
         return 0;
     }
 
     qsort(lines, count, sizeof(Line), compare_lines);
 
-    FILE *out = outfile ? fopen(outfile, "w") : stdout;
+    FILE *out = stdout;
+    if (outfile) {
+        out = fopen(outfile, "w");
+        if (!out) {
+            perror(outfile);
+            return 1;
+        }
+    }
+
     for (size_t i = 0; i < count; i++) {
         if (unique && i > 0 && compare_lines(&lines[i-1], &lines[i]) == 0) {
             free(lines[i].data);
@@ -83,3 +130,4 @@ int main(int argc, char *argv[]) {
     free(lines);
     return 0;
 }
+
