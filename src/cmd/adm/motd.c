fb600fbbcde0465a7c29037bcd7f0856b224802b
Charlie Root <root@xai.srclo.net>
2026-03-28 05:17:57 +0000

    motd

---
+++ src/cmd/adm/motd.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <pwd.h>
+#include <string.h>
+
+int main() {
+    // Get the UID of the user running the command
+    uid_t uid = getuid();
+    struct passwd *pw = getpwuid(uid);
+
+    if (!pw) {
+        fprintf(stderr, "Could not identify user.\n");
+        return 1;
+    }
+
+    char *user = pw->pw_name;
+
+    // Logic is now hardcoded and compiled, not read from a writable file
+    if (strcmp(user, "root") == 0) {
+        printf("Welcome to openXao. Copyright (C) 2026 The openXao Project\n");
+        printf("'You are root, be careful!.' - a wise man\n");
+    } else {
+        printf("Welcome to openXao, %s. Copyright (C) 2026 The openXao Project\n", user);
+    }
+
+    return 0;
+}
