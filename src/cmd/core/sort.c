d6f1bf294f114ececffdce7d94481126e011bcf1
Charlie Root <root@xai.srclo.net>
2026-03-28 00:14:52 +0000

    more posix for sort

--- src/cmd/core/sort.c
+++ src/cmd/core/sort.c
@@ -3,51 +3,83 @@
 #include <string.h>
 #include <unistd.h>
 
-/* POSIX 2024 requires handling arbitrary line lengths and large counts */
-typedef struct {
-    char *data;
-} Line;
+typedef struct { char *data; } Line;
+
+int rev = 1;
+int num = 0;
 
 int compare_lines(const void *a, const void *b) {
-    return strcmp(((Line *)a)->data, ((Line *)b)->data);
+    const char *s1 = ((Line *)a)->data;
+    const char *s2 = ((Line *)b)->data;
+    
+    if (num) {
+        return rev * (atof(s1) - atof(s2) > 0 ? 1 : (atof(s1) - atof(s2) < 0 ? -1 : 0));
+    }
+    return rev * strcmp(s1, s2);
+}
+
+void load_file(FILE *fp, Line **lines, size_t *count, size_t *cap) {
+    char *curr = NULL;
+    size_t len = 0;
+    while (getline(&curr, &len, fp) != -1) {
+        if (*count >= *cap) {
+            *cap *= 2;
+            *lines = realloc(*lines, *cap * sizeof(Line));
+        }
+        (*lines)[(*count)++].data = strdup(curr);
+    }
+    free(curr);
 }
 
 int main(int argc, char *argv[]) {
-    size_t cap = 1024;
-    size_t count = 0;
+    size_t cap = 1024, count = 0;
     Line *lines = malloc(cap * sizeof(Line));
-    int unique = 0;
-    int opt;
+    int unique = 0, check = 0, merge = 0, opt;
+    char *outfile = NULL;
 
-    /* Basic POSIX flag handling */
-    while ((opt = getopt(argc, argv, "u")) != -1) {
-        if (opt == 'u') unique = 1;
-    }
-
-    char *curr = NULL;
-    size_t len = 0;
-    
-    /* getline() is the POSIX way to handle arbitrary line lengths */
-    while (getline(&curr, &len, stdin) != -1) {
-        if (count >= cap) {
-            cap *= 2;
-            lines = realloc(lines, cap * sizeof(Line));
+    while ((opt = getopt(argc, argv, "nmcu ro:")) != -1) {
+        switch (opt) {
+            case 'n': num = 1; break;
+            case 'm': merge = 1; break; /* In memory, merge/sort are effectively similar */
+            case 'c': check = 1; break;
+            case 'u': unique = 1; break;
+            case 'r': rev = -1; break;
+            case 'o': outfile = optarg; break;
         }
-        lines[count++].data = strdup(curr);
     }
-    free(curr);
+
+    /* Process input operands */
+    if (optind == argc) {
+        load_file(stdin, &lines, &count, &cap);
+    } else {
+        for (int i = optind; i < argc; i++) {
+            FILE *fp = (strcmp(argv[i], "-") == 0) ? stdin : fopen(argv[i], "r");
+            if (!fp) continue;
+            load_file(fp, &lines, &count, &cap);
+            if (fp != stdin) fclose(fp);
+        }
+    }
+
+    if (check) {
+        for (size_t i = 1; i < count; i++) {
+            if (compare_lines(&lines[i-1], &lines[i]) > 0) return 1;
+        }
+        return 0;
+    }
 
     qsort(lines, count, sizeof(Line), compare_lines);
 
+    FILE *out = outfile ? fopen(outfile, "w") : stdout;
     for (size_t i = 0; i < count; i++) {
-        if (unique && i > 0 && strcmp(lines[i].data, lines[i-1].data) == 0) {
+        if (unique && i > 0 && compare_lines(&lines[i-1], &lines[i]) == 0) {
             free(lines[i].data);
             continue;
         }
-        fputs(lines[i].data, stdout);
+        fputs(lines[i].data, out);
         free(lines[i].data);
     }
 
+    if (outfile) fclose(out);
     free(lines);
     return 0;
 }
